8a1fde2ad4553331def8b285548b64f40f3f1d2b
CaiClone <caiskove@gmail.com>
2021-10-16 15:31:13 +0200

    [ComboTracker] Set combo UI

---
+++ Data/ComboStore.cs
@@ -0,0 +1,22 @@
+﻿
+using System.Collections.Generic;
+
+namespace GCDTracker.Data
+{
+    public static class ComboStore
+    {
+        public static Dictionary<uint, int[][]> COMBOS = new Dictionary<uint, int[][]>(){
+            //Gladiator
+            {1,new int[][] {
+                new int[] {9,15,21},
+                new int[] {7383,16457}
+            }},
+            //Paladin
+            {19,new int[][] {
+                new int[] {9,15,3539},
+                new int[] {9,15,3638},
+                new int[] {7383,16457},
+            }}
+        };
+    }
+}
--- PluginUI.cs
+++ PluginUI.cs
@@ -1,7 +1,10 @@
 ﻿using Dalamud.Game.ClientState;
+using Dalamud.Logging;
 using GCDTracker.Data;
 using ImGuiNET;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace GCDTracker
@@ -84,6 +87,57 @@ namespace GCDTracker
         }
         private void DrawComboLines()
         {
+            ImGui.Text($"{cs.LocalPlayer.ClassJob.Id}");
+            var xsep = 30*this.scale;
+            var ysep = 30 * this.scale;
+            int[][] combos;
+            var circRad = 8* this.scale;
+
+            if (ComboStore.COMBOS.TryGetValue(cs.LocalPlayer.ClassJob.Id, out combos))
+            {
+                var startPos = this.w_cent + new Vector2((this.w_size.X * 0.3f) + circRad + 2, -(combos.Length*ysep)/2);
+                var draw = ImGui.GetBackgroundDrawList();
+                for(uint i=0;i<combos.Length;i++)
+                {
+                    var combo = combos[i];
+                    var cpos = startPos+ new Vector2(0,i*ysep);
+                    var bifurc = false;
+                    for(uint j=0;j<combo.Length;j++)
+                    {
+                        if (i > 0 && combos[i - 1][j] == combo[j]) //skip if same start as previous combo
+                            bifurc = true;
+                        else
+                        {
+                            if (bifurc) //Draw previous line if it comes from another combo
+                            {
+                                cpos += new Vector2((xsep + circRad * 2)*j-1, 0);
+                                DrawBackLine(draw, cpos, xsep, ysep, circRad);
+                                bifurc = false;
+                            }
+                            cpos += new Vector2(xsep, 0);
+                            DrawActionCircle(draw, cpos, circRad);
+                            if (j < (combo.Length - 1))
+                                DrawFrontLine(draw, cpos, xsep, ysep, circRad);
+                            cpos += new Vector2(circRad*2, 0);
+                        }
+                    }
+                }
+            }
+        }
+        private void DrawBackLine(ImDrawListPtr draw,Vector2 cpos,float xsep, float ysep,float circRad)
+        {
+            draw.AddLine(cpos + new Vector2(xsep - circRad, 0), cpos - new Vector2(circRad + (circRad / 2), ysep - (circRad / 2)), ImGui.GetColorU32(conf.backColBorder), 5f * this.scale);
+            draw.AddLine(cpos + new Vector2(xsep - circRad, 0), cpos - new Vector2(circRad + (circRad / 2), ysep - (circRad / 2)), ImGui.GetColorU32(conf.backCol), 3f * this.scale);
+        }
+        private void DrawFrontLine(ImDrawListPtr draw, Vector2 cpos, float xsep, float ysep, float circRad)
+        {
+            draw.AddLine(cpos + new Vector2(circRad, 0), cpos + new Vector2(circRad+xsep, 0), ImGui.GetColorU32(conf.backColBorder), 5f * this.scale);
+            draw.AddLine(cpos + new Vector2(circRad, 0), cpos + new Vector2(circRad + xsep, 0), ImGui.GetColorU32(conf.backCol), 3f * this.scale);
+        }
+        private void DrawActionCircle(ImDrawListPtr draw, Vector2 cpos,float circRad)
+        {
+            draw.AddCircle(cpos, circRad, ImGui.GetColorU32(conf.backColBorder),20,5f*this.scale);
+            draw.AddCircle(cpos, circRad, ImGui.GetColorU32(conf.backCol), 20, 3f * this.scale);
         }
     }
 }
